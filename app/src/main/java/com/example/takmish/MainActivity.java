7f25afebdf5f1a94df4af07bae74a8c3ffcdb62b
Namrouti <Al_Namrouti@hotmail.com>
2022-04-20 09:51:39 +0300

    Initial commit

--- app/src/main/java/com/example/takmish/MainActivity.java
+++ app/src/main/java/com/example/takmish/MainActivity.java
@@ -1,14 +1,45 @@
 package com.example.takmish;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    // [START auth_fui_create_launcher]
+    // See: https://developer.android.com/training/basics/intents/result
+    private final ActivityResultLauncher<Intent> signInLauncher = registerForActivityResult(
+            new ActivityResultContract(),
+            new ActivityResultCallback<ActivityResultContract>() {
+                @Override
+                public void onActivityResult(ActivityResultContract result) {
+                    onSignInResult(result);
+                }
+            }
+    );
+    // [END auth_fui_create_launcher]
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
+
+
 }
